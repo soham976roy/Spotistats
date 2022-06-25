95be3f06eef25f91817549d3b43ee118b4a1c6bd
soham976roy <r.soham@iitg.ac.in>
2022-06-25 23:31:22 +0530

    commit

---
+++ client/src/App.js
@@ -0,0 +1,84 @@
+//import logo from './logo.svg';
+import { GlobalStyle } from './styles';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import { accessToken, logout,getCurrentUserProfile} from './spotify';
+import { useEffect, useState } from 'react';
+import { catchErrors } from './utils';
+import { Login, Profile, TopArtists, TopTracks, Playlists } from './pages';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  const [token,setToken]=useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
+  }, []);
+
+  
+  return (
+    <div className="app">
+      <GlobalStyle />
+
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+          <Router>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/top-tracks" element={<TopTracks/>}/>
+              <Route path="/playlists" element={<Playlists/>}/>
+              <Route path="/" element={<Profile/>}/>
+            </Routes>
+          </Router>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
